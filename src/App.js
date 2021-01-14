b664cac1f2c377ad1c7b0bfb9a7ed3111725648d
Shipix <shipix@hotmail.fr>
2021-01-14 11:00:00 +0100

    Modification du component App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import Logo from './logoshipix.png';
+import './style.css'; 
 
 function App() {
   return (
     <div >
-     
+     <div>
+       <h1>
+         Exercices 3
+       </h1>
+       <img src={Logo} alt=""height="200px"/>
+     </div>
     </div>
   );
 }
